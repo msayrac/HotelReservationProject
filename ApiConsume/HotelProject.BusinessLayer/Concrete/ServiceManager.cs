dd83e56477371a309df2e77b07fa66335b07ca9a
msayrac <msayracode@gmail.com>
2024-02-24 14:27:03 +0300

    Services and Manager added

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+	public class ServiceManager : IServiceService
+	{
+
+		private readonly IServiceDal _serviceDal;
+
+		public ServiceManager(IServiceDal serviceDal)
+		{
+			_serviceDal = serviceDal;
+		}
+
+		public void TInsert(Service t)
+		{
+			_serviceDal.Insert(t);
+		}
+
+		public void TDelete(Service t)
+		{
+			_serviceDal.Delete(t);
+		}
+
+		public void TUpdate(Service t)
+		{
+			_serviceDal.Update(t);
+		}
+
+		public List<Service> TGetList()
+		{
+			return _serviceDal.GetList();
+		}
+
+		public Service TGetByID(int id)
+		{
+			return _serviceDal.GetByID(id);
+		}
+	}
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+	public class Context : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=msyc; initial catalog=ApiDb; integrated security=true; TrustServerCertificate=True");
+
+		}
+
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<Service> Services { get; set; }
+		public DbSet<Staff> Staffs { get; set; }
+		public DbSet<Subscribe> Subscribes { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+
+
+
+
+
+
+
+
+
+
+	}
+}
