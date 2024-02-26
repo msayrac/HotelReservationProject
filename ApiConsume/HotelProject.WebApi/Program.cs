0e2ed40e4558edd7f2de6246a570ba9ae5606a87
msayrac <msayracode@gmail.com>
2024-02-26 21:32:28 +0300

    staff added in frontend

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -26,18 +26,19 @@ builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
 builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
 builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
+builder.Services.AddCors((opt =>
+{
+	opt.AddPolicy("OtelApiCors", opts =>
+	{
+		opts.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+	});
+}));
 
 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-
-
-
-
-
 
 
 builder.Services.AddSwaggerGen();
@@ -51,6 +52,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
+app.UseCors("OtelApiCors");
 app.UseAuthorization();
 
 app.MapControllers();
--- Frontend/HotelReservationProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelReservationProject.WebUI/Controllers/StaffController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using HotelReservationProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelReservationProject.WebUI.Controllers
 {
 	public class StaffController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public StaffController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:33170/api/Staff");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+
+
+		[HttpGet]
+		public IActionResult AddStaff()
 		{
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddStaff(AddStaffViewModel model)
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var jsonData = JsonConvert.SerializeObject(model);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+			var responseMessage = await client.PostAsync("http://localhost:33170/api/Staff", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+
+
+
+
+
+
+
+
+
 	}
 }
