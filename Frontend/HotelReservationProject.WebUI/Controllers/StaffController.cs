4b386a1685c638c438f3b38b89c1c51f250f7aa2
msayrac <msayracode@gmail.com>
2024-02-28 12:18:59 +0300

    Staff update and delete added

--- Frontend/HotelReservationProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelReservationProject.WebUI/Controllers/StaffController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using HotelReservationProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace HotelReservationProject.WebUI.Controllers
@@ -56,15 +57,51 @@ namespace HotelReservationProject.WebUI.Controllers
 		public async Task<IActionResult> DeleteStaff(int id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.DeleteAsync($"http://localhost:33170/api/Staff?StaffID={id}");
+			var responseMessage = await client.DeleteAsync($"http://localhost:33170/api/Staff/{id}");
 
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
 			}
 			return View();
-
 		}
+
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateStaff(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var responseMessage = await client.GetAsync($"http://localhost:33170/api/Staff/{id}");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel model)
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var jsonData = JsonConvert.SerializeObject(model);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+			var responseMessage = await client.PutAsync("http://localhost:33170/api/Staff", stringContent);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+
+
 
 
 
