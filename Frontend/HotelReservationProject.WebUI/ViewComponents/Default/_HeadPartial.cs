04dba8a44efe0ce087b15fb1f652fa0802769857
msayrac <msayracode@gmail.com>
2024-03-04 10:02:26 +0300

    Frontend default partial added

---
+++ Frontend/HotelReservationProject.WebUI/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationProject.WebUI.ViewComponents.Default
+{
+    public class _HeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Frontend/HotelReservationProject.WebUI/ViewComponents/Default/_SpinnerPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationProject.WebUI.ViewComponents.Default
+{
+	public class _SpinnerPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
