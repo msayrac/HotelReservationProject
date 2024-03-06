ce6b5e6ab2229824b53517967b7116335faaebd0
msayrac <msayracode@gmail.com>
2024-03-06 21:25:56 +0300

    Services dto added

--- Frontend/HotelReservationProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ Frontend/HotelReservationProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelReservationProject.WebUI.Dtos.RoomDto;
+using HotelReservationProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelReservationProject.WebUI.ViewComponents.Default
 {
 	public class _ServicePartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:33170/api/Service");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
 }
