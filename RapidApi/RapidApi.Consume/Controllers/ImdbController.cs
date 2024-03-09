863eebd7f762b1ca3432d7513b189e0622370bdb
msayrac <msayracode@gmail.com>
2024-03-09 21:14:42 +0300

    Imdb rapid api added

---
+++ RapidApi/RapidApi.Consume/Controllers/ImdbController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApi.Consume.Models;
+using System.Net.Http.Headers;
+
+namespace RapidApi.Consume.Controllers
+{
+	public class ImdbController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
+
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "fca0ab1c87msh2c00b12fe3e5e15p1ef69fjsn35e5b3dae135" },
+		{ "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+				return View(apiMovieViewModels);
+
+			}
+
+
+
+
+
+
+		}
+	}
+}
