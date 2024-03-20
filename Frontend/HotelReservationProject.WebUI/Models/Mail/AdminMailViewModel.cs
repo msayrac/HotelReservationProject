0f7daf14c6f50b8b455ded753a4eef3b584c0ee7
msayrac <msayracode@gmail.com>
2024-03-20 22:55:20 +0300

    Mail send and passkey created

---
+++ Frontend/HotelReservationProject.WebUI/Models/Mail/AdminMailViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelReservationProject.WebUI.Models.Mail
+{
+	public class AdminMailViewModel
+	{
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+
+
+
+
+    }
+}
