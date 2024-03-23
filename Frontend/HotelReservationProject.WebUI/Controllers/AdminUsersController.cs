d8bc6e1f11e4eb13b07004ecff15ca0aed971ebc
msayrac <msayracode@gmail.com>
2024-03-23 17:35:09 +0300

    AdminUser added

---
+++ Frontend/HotelReservationProject.WebUI/Controllers/AdminUsersController.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationProject.WebUI.Controllers
+{
+	[AllowAnonymous]
+	public class AdminUsersController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public AdminUsersController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public IActionResult Index()
+		{
+			var values = _userManager.Users.ToList();
+
+			return View(values);
+		}
+	}
+}
